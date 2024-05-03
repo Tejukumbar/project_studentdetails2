da274580ff2cd0e3f22a3cf2d56ed1cdc3f69eda
Tejukumbar <tejukumbar572@gmail.com>
2024-05-03 15:17:39 +0530

    program to display array list

---
+++ FruitsArray.java
@@ -0,0 +1,10 @@
+public class FruitsArray {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Grapes", "Mango"};
+
+        System.out.println("Array of Fruits:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
