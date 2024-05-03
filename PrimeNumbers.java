4933819c240f3174209e9a4b726072a4c943cce4
Tejukumbar <tejukumbar572@gmail.com>
2024-05-03 15:09:12 +0530

    program to display prime numbers

---
+++ PrimeNumbers.java
@@ -0,0 +1,29 @@
+public class PrimeNumbers {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printPrimeNumbers(int start, int end) {
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int start = 2; // Start of the range
+        int end = 50; // End of the range
+        printPrimeNumbers(start, end);
+    }
+}
